a43cb966f5aac8f685d53c17114aed1199a879ef
relnod <mail@paul-schiffers.de>
2018-12-27 15:07:35 +0100

    api/server: add some more debug/pprof endpoints

--- api/server/server.go
+++ api/server/server.go
@@ -49,11 +49,15 @@ func (s *Server) Start() error {
 	r.HandleFunc("/ticks", s.handleSetTicks).Methods("POST")
 
 	if s.debug {
-		r.HandleFunc("/debug/pprof/", pprof.Index).Methods("GET")
-		r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline).Methods("GET")
-		r.HandleFunc("/debug/pprof/profile", pprof.Profile).Methods("GET")
-		r.HandleFunc("/debug/pprof/symbol", pprof.Symbol).Methods("GET")
-		r.HandleFunc("/debug/pprof/trace", pprof.Trace).Methods("GET")
+		r.HandleFunc("/debug/pprof/", pprof.Index)
+		r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+		r.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+		r.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+		r.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+		r.Handle("/debug/pprof/block", pprof.Handler("block"))
 	}
 
 	err := http.ListenAndServe(s.addr, r)
