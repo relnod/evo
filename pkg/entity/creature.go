8a652b0a24cd3b8036a197753c903465232fa033
relnod <mail@paul-schiffers.de>
2019-01-07 14:46:16 +0100

    entity/creature: stabilize simulation

--- pkg/entity/creature.go
+++ pkg/entity/creature.go
@@ -84,7 +84,7 @@ func newCreature(pos math64.Vec2, radius float64, brain *deep.Neural, generation
 	var newEyes []*Eye
 	// energyConsumption := mutate(rand.Float64()*radius, 0.1, 0.1)
 
-	energyConsumption := (rand.NormFloat64()*0.1 + 1.0) / 300.0 * ((1.0*radius + 0.0*(radius*radius)) / 4)
+	energyConsumption := (rand.NormFloat64()*0.1 + 1.0) / 300.0 * (math64.Poly(radius, 0, 1, 0.1) / 4)
 	energy := radius
 
 	// if radius > 4.0 {
@@ -104,11 +104,11 @@ func newCreature(pos math64.Vec2, radius float64, brain *deep.Neural, generation
 				newEyes = append(newEyes, NewEye(eyeRange, eye.Detects))
 			}
 
-			// With a 10% chance a new eye appears.
+			// With a 2% chance a new eye appears/disapears
 			r := rand.Float64()
-			if r > 0.9 {
+			if r > 0.98 {
 				newEyes = append(newEyes, NewRandomEye())
-			} else if r < 0.1 && len(newEyes) > 1 {
+			} else if r < 0.02 && len(newEyes) > 1 {
 				newEyes = newEyes[:len(newEyes)-1]
 			}
 		}
